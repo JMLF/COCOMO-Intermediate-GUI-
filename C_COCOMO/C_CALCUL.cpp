685868269158df6b00255b06f0456b4179054c37
Marius <49654973+JMLF@users.noreply.github.com>
2022-02-11 08:56:09 +0100

    début code class

--- C_COCOMO/C_CALCUL.cpp
+++ C_COCOMO/C_CALCUL.cpp
@@ -1,4 +1,47 @@
 #include "C_CALCUL.h"
 
+C_CALCUL::C_CALCUL(TypeDeProjet type)
+{
+	switch (type)
+	{
 
+	case C_CALCUL::TypeDeProjet::ORGANIC:
+		TypeProjetInt = 0;
+		break;
+	case C_CALCUL::TypeDeProjet::DETACHED:
+		TypeProjetInt = 1;
+		break;
+	case C_CALCUL::TypeDeProjet::EMBEDDED:
+		TypeProjetInt = 2;
+		break;
+	default:
+		TypeProjetInt = 0;
+		break;
+	}
 
+}
+
+void C_CALCUL::setKLOC(float KLOC)
+{
+	this->KLOC = KLOC;
+}
+
+void C_CALCUL::setEAF(float EAF)
+{
+	this->EAF = EAF;
+}
+
+float C_CALCUL::calcul_E()
+{
+	return 0;
+}
+
+float C_CALCUL::calcul_D(float E)
+{
+	return 0;
+}
+
+float C_CALCUL::calcul_P(float E, float D)
+{
+	return 0;
+}
--- C_COCOMO/C_CALCUL.h
+++ C_COCOMO/C_CALCUL.h
@@ -4,10 +4,44 @@
 
 class C_CALCUL
 {
+protected: 
+	
+	//constante des projets organique 
+	const float organic_A = 2.4;
+	const float organic_B = 1.05;
+	const float orgnic_C = 0.38;
 
+	//constante des projets semi-détaché
+	const float detached_A = 3.0;
+	const float detached_B = 1.12;
+	const float detached_C = 0.35;
 
+	//constante des projets imbriqué
+	const float embedded_A = 3.6;
+	const float embedded_B = 1.20;
+	const float embedded_C = 0.32;
 
+	float KLOC; //nombres de lignes de code (en millier)
+	float EAF; //effort ajustement factor
+	
+	int TypeProjetInt;
 
+	float E; //Effort
+	float D; //Developpement time
+	float P; //most effective number of persons 
+
+public:
+	
+	enum class TypeDeProjet { ORGANIC, DETACHED,EMBEDDED};
+
+	C_CALCUL(TypeDeProjet type = TypeDeProjet::ORGANIC);
+
+	void setKLOC(float KLOC);
+	void setEAF(float EAF);
+
+	float calcul_E();
+	float calcul_D(float E);
+	float calcul_P(float E, float D);
 
 };
 
